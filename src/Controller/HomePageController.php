91aaab3dafaaa7735d543b49eb544e0b75a1f93c
Egor Danilkin <egr13.2011@gmail.com>
2021-11-05 00:54:43 +0700

    fix

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
 
 class HomePageController extends AbstractController
 {
-    /**
-     * @Route('/', name: 'home_page')
-     */
+
+     #[Route('/', name: 'home_page')]
+
     public function index(): Response
     {
+
         $productRepository = $this->getDoctrine()->getRepository(Product::class);
         $products = $productRepository->findAll();
 
